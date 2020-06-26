f9b33cca9e90bfe09df1f8fb6583196083b719ff
codystack <ebuzzadvert@gmail.com>
2020-06-26 11:46:28 +0100

    kaydex modified

--- kaydex.php
+++ kaydex.php
@@ -8,7 +8,7 @@
             <div class="row justify-content-center">
                 <div class="col-lg-10">
                     <div class="details_info ">
-                        <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".3s">Kayode Micheal</h3>
+                        <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".3s">Kayode Micheal Fayehun</h3>
                         <div class="details_links d-flex justify-content-between align-items-center">
                             <div class="single_details single_input wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
                                 <span>Stage Name</span>
@@ -38,7 +38,6 @@
                                     <a href="https://audiomack.com/iamkaydexx" target="_blank" class="boxed-btn3">View on Audiomack</a>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -57,10 +56,37 @@
                     <div class="details_text">
                         <div class="row">
                             <div class="col-lg-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
-                                <p>Kayode Michael Fayehun (born 23 September 1993) better known by his stage name as KAYDEX is a Nigerian singer and songwriter originally from Ondo State, Nigeria. He currently resides in Lagos, Nigeria, and he's signed to KH Records (he's co-CEO and a major stakeholder). Emerging as a charismatic singer from Lagos, Kaydex has quickly risen to become one of Afrobeats’ most promising upstarts.</p>
+                                <p>Afro-centric multi Talented Nigerian music artists {Kayode Michael Fayehun} Porpularly known as “KAYDEX”. Was born  on September 23, 1993, KAYDEX is
+                                originally from Ondo town, Ondo state Nigeria born in Ketu Lagos. He attended The Nigerian Military School {NMS}
+                                Zaria... 
+
+                                His foray into music in 2009 was motivated by the desire to continue
+                                his grandfather GENERAL ADEKUNLE's legacy. General Adekunle was a
+                                Nigerian Juju musician and is one of His creative influences.</p>
                             </div>
                             <div class="col-lg-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
-                                <p>The self-assured singer began making music while growing up in a far-flung suburb of Lagos. Kaydex’s music embodies the overwhelming energy of this era; he has also featured the likes of Reminisce etc. His groovy take on Afropop is inspired by the boisterous music of Fela and others.</p>
+                                <p>Kaydex is also a songwriter and employs the use of creative African
+                                lyrics to deliver truly original sounds.
+
+                                His debut singles "EGWUEJI" was released officially on the 8th of
+                                August and "TELEMI" audio was released october 14 2016 and was
+                                produced by FLIPTYCE. Videos has also been released for this wonderful
+                                materials as well.</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="thumbs">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6">
+                                <div class="single_thumb wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".4s">
+                                    <img src="https://i.imgur.com/mnClThE.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="single_thumb wow fadeInRight" data-wow-duration="1s" data-wow-delay=".4s">
+                                    <img src="https://i.imgur.com/rM1ikMW.jpg" alt="">
+                                </div>
                             </div>
                         </div>
                     </div>
